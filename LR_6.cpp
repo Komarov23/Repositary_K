959fab49ed76282bc32f32d90a9a76d7ea1385c0
Komarov23 <komarov2004qw@gmail.com>
2021-10-22 21:35:38 +0300

    Create LR_6.cpp

---
+++ LR_6.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    int max = 3;
+    float Y = 0;
+    int x;
+
+    cout << "Введіть значення : ";
+    cin >> x;
+
+
+    float i = 2;
+    while (i <= max) {
+        Y += pow(cos(x), 2) + log10(x) + 2;
+        cout << "i= " << i << "\t" << "Y= " << Y << endl;
+        i += 0.1;
+    }
+
+   
+}
